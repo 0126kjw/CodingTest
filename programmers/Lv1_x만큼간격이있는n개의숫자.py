091e6ff03d33c0a1a8fd4c2b87a63a4eb15c5b40
Jinwoo Kim <0126kjw@kakao.com>
2022-10-09 22:57:11 +0900

    Solve Lv1_x만큼간격이있는n개의숫자

--- programmers/Lv1_x만큼간격이있는n개의숫자.py
+++ programmers/Lv1_x만큼간격이있는n개의숫자.py
@@ -7,9 +7,14 @@
 # x는 -10000000 이상, 10000000 이하인 정수입니다.
 # n은 1000 이하인 자연수입니다.
 
-def solution(arr):
-    answer =  []
+def solution(x, n):
+    answer = []
+    for i in range(1,n+1):
+        answer.append(x * i)
     return answer
+
+print(solution(2, 5))  # [2,4,6,8,10]
+print(solution(4, 3))  # [4,8,12]
 
 
 # 위 문제의 저작권은 2022 프로그래머스 (주)그렙 에 있습니다.
