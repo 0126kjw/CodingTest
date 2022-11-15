88945bcf292790a9bc098abbc98440faf2a50fc5
Jinwoo Kim <0126kjw@kakao.com>
2022-11-15 17:05:01 +0900

    Solve Lv1_약수의개수와덧셈.py

--- programmers/Lv1_약수의개수와덧셈.py
+++ programmers/Lv1_약수의개수와덧셈.py
@@ -6,8 +6,16 @@
 # 제한사항
 # 1 ≤ left ≤ right ≤ 1,000
 
-def solution(s):
+def solution(left, right):
     answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
     return answer
+
+print(solution(13, 17))  # 43
+print(solution(24, 27))  # 52
 
 # 위 문제의 저작권은 2022 프로그래머스 (주)그렙 에 있습니다.
