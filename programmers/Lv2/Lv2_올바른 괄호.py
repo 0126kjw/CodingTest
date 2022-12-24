f752ce8b53bf992fb79e220cf0936b2e029017ff
Jinwoo Kim <0126kjw@kakao.com>
2022-12-25 00:02:43 +0900

    Solve Lv2_올바른 괄호.py

--- programmers/Lv2/Lv2_올바른 괄호.py
+++ programmers/Lv2/Lv2_올바른 괄호.py
@@ -11,11 +11,22 @@
 # 문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
 
 def solution(s):
-    answer = True
-    
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
+    stack = []
+    for i in s:
+        if i == "(":
+            stack.append(i)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+    if stack:
+        return False
     return True
+
+print(solution("()()"))  # true
+print(solution("(())()"))  # true
+print(solution(")()("))  # false
+print(solution("(()("))  # false
 
 # 위 문제의 저작권은 2022 프로그래머스 (주)그렙 에 있습니다.
