2c123178fee0b8a2fc22b323a48f6e65aae3708f
Jinwoo Kim <0126kjw@kakao.com>
2023-02-05 01:37:22 +0900

    Solve Lv1_행렬의덧셈.py

---
+++ programmers/Lv1/Lv1_행렬의덧셈.py
@@ -0,0 +1,20 @@
+# 작성 날짜: 23.02.05
+
+# 문제 설명
+# 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+
+# 제한 조건
+# 행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
+
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        ans_sum = []
+        for j in range(len(arr1[0])):
+            ans_sum.append(arr1[i][j] + arr2[i][j])
+        answer.append(ans_sum)
+    return answer
+
+print(solution([[1,2],[2,3]], [[3,4],[5,6]]))  # [[4,6],[7,9]]
+
+# 위 문제의 저작권은 2023 프로그래머스 (주)그렙 에 있습니다.
