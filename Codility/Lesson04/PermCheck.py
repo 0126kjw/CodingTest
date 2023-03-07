5bcaa3eec6625875cefe2c1e3b2ef4ffdc4e79bf
Jinwoo Kim <0126kjw@kakao.com>
2023-03-08 00:03:51 +0900

    Solve PermCheck.py

---
+++ Codility/Lesson04/PermCheck.py
@@ -0,0 +1,66 @@
+# Task description
+# A non-empty array A consisting of N integers is given.
+
+# A permutation is a sequence containing each element from 1 to N once, and only once.
+
+# For example, array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# is a permutation, but array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# is not a permutation, because value 2 is missing.
+
+# The goal is to check whether array A is a permutation.
+
+# Write a function:
+
+# def solution(A)
+
+# that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+# For example, given array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# the function should return 1.
+
+# Given array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# the function should return 0.
+
+# Write an efficient algorithm for the following assumptions:
+
+# N is an integer within the range [1..100,000];
+# each element of array A is an integer within the range [1..1,000,000,000].
+
+
+
+# My Code
+def solution(A):
+    answer = []
+    num = sorted(A)
+    for i in range(1,len(A)+1):
+        answer.append(i)
+    if num == answer:
+        return 1
+    else:
+        return 0
+    
+
+# Result
+# https://app.codility.com/demo/results/training8SMPZ2-QCQ/
+
+
+
+# Copyright 2009–2023 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
