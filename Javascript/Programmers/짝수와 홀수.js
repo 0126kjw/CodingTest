d84908d3d28b737d67fcf2928ba7e854004d08ec
Jinwoo Kim <0126kjw@kakao.com>
2023-03-17 00:02:47 +0900

    Solve 짝수와 홀수 with JS

---
+++ Javascript/Programmers/짝수와 홀수.js
@@ -0,0 +1,17 @@
+// 문제 설명
+// 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+
+// 제한 조건
+// num은 int 범위의 정수입니다.
+// 0은 짝수입니다.
+
+// MY CODE
+function solution(num) {
+    if (num % 2 == 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+// 위 문제의 저작권은 2023 프로그래머스 (주)그렙 에 있습니다.
